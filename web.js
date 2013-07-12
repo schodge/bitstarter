b2274d2960b41f24b99355c0d3d4f3d2dfb11a97
Ubuntu <ubuntu@ip-10-164-50-10.ec2.internal>
2013-07-12 22:15:11 +0000

    index.html read

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var textToDisplay = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(buf.toString(textToDisplay));
+    response.send(textToDisplay.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
